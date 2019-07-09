b8f36679da8307793ee210cd823722d554de0428
Jamie Barton <jbarton10@email.arizona.edu>
2019-07-09 10:53:45 -0700

    Concert Function working

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,33 +1,56 @@
 require("dotenv").config()
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+//Spotify is undefined?
+//var spotify = new Spotify(keys.spotify);
+var axios = require('axios');
+//Getting moment.js in
+var moment = require('moment');
+moment().format();
 
 var userInput = process.argv[2];
 
 
-//Maybe switch these to switch cases
-if (userInput.toLowerCase() == "concert-this"){
-    concertFunction();
-}
-else if(userInput.toLowerCase() == "spotify-this-song"){
-    spotifyFunction();
+//Switch case to check input
+switch(userInput.toLowerCase()) {
+    case "concert-this":
+        concertFunction();
+        break;
 
-}
-else if(userInput.toLowerCase() == "movie-this"){
-    movieFunction();
+    case "spotify-this-song":
+        spotifyFunction();
+        break;
 
-}
-else if(userInput.toLowerCase() == "do-what-it-says"){
-    doFunction();
-}
-else{
-    console.log("Please enter a valid command.")
-}
+    case "movie-this":
+        movieFunction();
+        break;
+
+    case "do-what-it-says":
+        doFunction();
+        break;
+
+    default:
+        console.log("Please enter a valid command.");
+        break;
+  }
 
 
 //Functions
 function concertFunction(){
     var artist = process.argv[3];
+    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+   
+    console.log(url);
+
+    axios.get(url).then(function(response){
+        console.log(response.data);
+        for(var i =0; i< response.data.length; i++){
+            console.log("Name of venue: " +response.data[i].venue.name);
+            console.log("Located in " +response.data[i].venue.city + " " + response.data[i].venue.country);
+            var date = moment(response.data[i].datetime).format("MM/DD/YYYY HH:mm:ss");
+            console.log("Date of concert: " +date + "\n");
+        }
+
+    });
 
 }
 function spotifyFunction(){
